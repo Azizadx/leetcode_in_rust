6e0a10f4529202b0468f0dfbe7d59ad29187cb73
Nasrallah Hassan <craft@azizadx.me>
2024-05-11 01:15:48 +0300

    Time: 14 ms (49.59%) | Memory: 3.9 MB (36.18%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.rs
+++ 217-contains-duplicate/contains-duplicate.rs
@@ -1,8 +1,8 @@
+use std::collections::HashSet;
 impl Solution {
+    #[inline]
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        use std::collections::HashSet;
-        let mut seen = HashSet::new();
-
+        let mut seen = HashSet::<i32>::new();
         for num in nums {
             if seen.contains(&num) {
                 return true;
