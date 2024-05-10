35d1a87144234c3f21261af71fb79d0c55de55a3
Nasrallah Hassan <craft@azizadx.me>
2024-05-11 00:20:04 +0300

    Time: 2 ms (60.12%) | Memory: 2.4 MB (39.59%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.rs
@@ -0,0 +1,43 @@
+impl Solution {
+    #[inline]
+    pub fn is_palindrome(s: String) -> bool {
+        //convert the string to char
+        let chars: Vec<char> = s.chars().collect();
+        let mut left_pointer = 0;
+        let mut right_pointer = chars.len() - 1;
+
+        while left_pointer < right_pointer {
+            // Skip non-alphanumeric from left pointer
+            if !Solution::is_alphanumeric(chars[left_pointer]) {
+                left_pointer +=1;
+                continue;
+            }
+            // Skip non-alphanumeric from right pointer
+            if !Solution::is_alphanumeric(chars[right_pointer]) {
+                right_pointer -=1;
+                continue;
+            }
+
+            if Solution::is_lowercase(chars[left_pointer]) != Solution::is_lowercase(chars[right_pointer]){
+                return false;
+            }
+
+            left_pointer += 1;
+            right_pointer -=1;
+        }
+        true
+    }
+    // helper functions
+
+    fn is_alphanumeric(c: char) -> bool {
+        (c >= 'a' && c<='z') ||  (c >= 'A' && c<='Z') ||  (c >= '0' && c<='9')
+    }
+    fn is_lowercase(c: char) -> char {
+        if c >= 'A' && c <= 'Z' {
+            return (c as u8 + 32) as char;
+        }
+        c
+    }
+
+    
+}
