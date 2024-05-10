1e511ea7cb4293dfb02e7bc26c6c08d0586c1237
Nasrallah Hassan <craft@azizadx.me>
2024-05-11 01:17:35 +0300

    Time: 10 ms (85.85%) | Memory: 3.8 MB (56.10%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.rs
+++ 217-contains-duplicate/contains-duplicate.rs
@@ -4,10 +4,9 @@ impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
         let mut seen = HashSet::<i32>::new();
         for num in nums {
-            if seen.contains(&num) {
-                return true;
+            if !seen.insert(num) {
+                return true
             }
-            seen.insert(num);
         }
         false
     }
