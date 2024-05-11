3eefcac61507f803f7cb7609834c40a197733e24
Nasrallah Hassan <craft@azizadx.me>
2024-05-11 03:08:31 +0300

    Time: 3 ms (75.20%) | Memory: 2.3 MB (36.39%) - LeetSync

---
+++ 792-binary-search/binary-search.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len() as i32 - 1;
+
+        while left <= right {
+            //calculate the mid value
+            let mid = (right + left)/2;
+
+            if nums[mid as usize] == target {
+                return mid;
+            }
+            else if nums[mid as usize] < target {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        -1 // if the number is does not exist in array
+    }
+}
