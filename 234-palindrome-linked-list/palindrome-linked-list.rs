328c7114b45aef1aa761edc3f627fc988ec10030
Nasrallah Hassan <craft@azizadx.me>
2024-05-21 09:20:42 +0300

    Time: 41 ms (92.76%) | Memory: 9.1 MB (31.25%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.rs
@@ -0,0 +1,25 @@
+use crate::list_node::ListNode;
+impl Solution {
+    pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
+
+        let mut values = Vec::new();
+        let mut current = &head; // reference to head of linked list
+
+        // traverses the linked list.
+        while let Some(node) = current {
+            values.push(node.val);
+            current = &node.next;
+        }
+
+        // Check if the values form a palindrome
+        let len = values.len();//length of the vector 
+        //loop over the 1st half
+        for i in 0..len / 2 {
+            if values[i] != values[len - 1 - i] {
+            return false; // which mean that is not plindrome
+        }
+    }
+    true   
+    }
+
+}
