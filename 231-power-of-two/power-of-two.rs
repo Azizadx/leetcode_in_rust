b41d9a46296cf8f8382c76d6ab26f6d0f30c4475
Nasrallah Hassan <craft@azizadx.me>
2024-05-21 09:05:53 +0300

    Time: 0 ms (100.00%) | Memory: 2 MB (100.00%) - LeetSync

--- 231-power-of-two/power-of-two.rs
+++ 231-power-of-two/power-of-two.rs
@@ -5,14 +5,12 @@ e.g: n = 4
 n & (n - 1)
 n -1 = 3 = 0011
 0100 & 0011 = 0000 which is true
-
 e.g: 3 = 0011
 n - 1 = 2 = 0010
 0011 & 0010 = 0010 there 1 which is false
-
 */
 impl Solution {
     pub fn is_power_of_two(n: i32) -> bool {
-        n > 0 && (n & (n - 1)) == 0
+        n > 0 && n & (n - 1) == 0
     }
 }
